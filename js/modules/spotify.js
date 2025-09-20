a640b77098377a8df2361876004f4aa7837af5af
hbtmarc <herbertmarck@gmail.com>
2025-09-19 23:38:25 -0300

    Fix Spotify

--- js/modules/spotify.js
+++ js/modules/spotify.js
@@ -178,4 +178,11 @@ function generateCodeVerifier(length) {
     return text;
 }
 
-async
+async function generateCodeChallenge(codeVerifier) {
+    const data = new TextEncoder().encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest('SHA-256', data);
+    return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+  .replace(/\+/g, '-')
+  .replace(/\//g, '_')
+  .replace(/=+$/, '');
+}
